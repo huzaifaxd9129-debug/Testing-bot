67bc3ccb1d0aab97461bb67d562e57bb33f12ab3
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-15 16:31:18 +0500

    commandhandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,20 +1,17 @@
 const fs = require("fs");
-const path = require("path");
 
 module.exports = (client) => {
   const folders = fs.readdirSync("./commands");
 
   for (const folder of folders) {
-    const files = fs
-      .readdirSync(`./commands/${folder}`)
-      .filter(f => f.endsWith(".js"));
+    const files = fs.readdirSync(`./commands/${folder}`);
 
     for (const file of files) {
-      const command = require(`../commands/${folder}/${file}`);
+      const cmd = require(`../commands/${folder}/${file}`);
 
-      if (command.name) {
-        client.commands.set(command.name, command);
-        console.log(`Loaded: ${command.name}`);
+      if (cmd.name) {
+        client.commands.set(cmd.name, cmd);
+        console.log("Loaded:", cmd.name);
       }
     }
   }
